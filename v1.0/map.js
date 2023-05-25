30507baf14d49d1be56a1a17fa5a820916312508
Charan <2100031094@kluniversity.in>
2023-05-25 18:08:42 +0530

    google tag

--- v1.0/map.js
+++ v1.0/map.js
@@ -79,12 +79,3 @@ function cha(a)
     console.log("hi")
 }
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y39QJDMJ28"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-Y39QJDMJ28');
-</script>
